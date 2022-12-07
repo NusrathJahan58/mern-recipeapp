40ea2a6473577ae5d4aab11b79a37eec27ebf24a
NusrathJahan58 <nusrathjahan58@gmail.com>
2022-12-07 04:15:04 +0100

    first commit

---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,21 @@
+const Footer = () => {
+	return (
+		<footer id='footer' className='footer'>
+			<div className='container'>
+				
+				<div className='footer__developer' >
+					<p>
+						Foodie Guide is a web application developed by{' '}
+						<a href='mailto:nusrathjahan58@gmail.com'>
+							<strong>Nusrath Jahan</strong>
+						</a> 
+						
+					</p>
+					<p>&copy; 2022. All rights reserved.</p>
+				</div>
+			</div>
+		</footer>
+	)
+}
+
+export default Footer
